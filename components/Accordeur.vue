8e3caa93194cdbd159089ab45fc260df5aad91a3
yohannmarchand <marchandyohannnnn@gmail.com>
2023-01-20 10:53:14 +0100

    feat: 🎸 Connect meter with guitar view

--- components/Accordeur.vue
+++ components/Accordeur.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="flex flex-col space-y-4">
+  <div class="flex flex-col space-y-16">
+    <div class="text-center text-3xl text-zinc-400 font-bold">{{ input }}</div>
+
     <FrenquencyMeter
-      :value="Math.trunc(input)"
-      :rightValue="rightValue"
+      :value="value"
+      :rightValue="correctValue"
       :range="range"
     />
 
     <!-- Test: wait for reel data -->
-    <div class="mt-6 mx-auto">
+   <!-- <div class="mt-6 mx-auto">
       <input v-model="input" type="range" :min="rightValue - (range / 2)" :max="(range / 2) + rightValue">
     </div>
     <div class="text-center">
       Value : {{ input }}
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -30,14 +32,24 @@ export default {
   data() {
     return {
       input: 0,
-      range: 400,
+      range: 200,
       url: 'ws://172.20.10.7/ws',
     }
   },
 
+  computed: {
+    value() {
+      return Math.trunc(this.input);
+    },
+
+    correctValue() {
+      return this.rightValue
+    }
+  },
+
   watch: {
-    rightValue() {
-      this.input = Math.ceil(this.rightValue) - (this.range / 2)
+    correctValue() {
+      this.input = Math.ceil(this.correctValue) - (this.range / 2)
     }
   },
 

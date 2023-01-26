07f56d2bb0e1adb783c334699e66d3d4f818a909
yohannmarchand <marchandyohannnnn@gmail.com>
2023-01-26 11:45:38 +0100

    feat: 🎸 Display the note that are tunned

---
+++ components/SelectingNote.vue
@@ -0,0 +1,11 @@
+<template>
+  <div v-if="note" class="text-xl font-bold text-neutral-400">You are tunning the note : <span class="text-2xl">{{ note.name }}</span> </div>
+</template>
+
+<script>
+export default {
+  props: {
+    note: String
+  }
+}
+</script>

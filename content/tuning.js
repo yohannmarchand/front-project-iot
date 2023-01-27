e0ea6cb6b4936928b1733269f97bd16de8f86f0d
yohannmarchand <marchandyohannnnn@gmail.com>
2023-01-27 16:02:51 +0100

    fix: 🐛 Audio assets not found

--- content/tuning.js
+++ content/tuning.js
@@ -1,5 +1,5 @@
 export default {
-  "Base": ["E2", "A2", "D3", "G3", "B3", "E4"],
+  "Standard": ["E2", "A2", "D3", "G3", "B3", "E4"],
   /*"1 ton": "DGCFAD",
   "Drop d": "DADGBE",
   "Double dropped d": "DADGBD",

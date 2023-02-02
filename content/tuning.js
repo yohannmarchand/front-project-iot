e6fa449908b32929749e82294e29b42370d48f07
yohannmarchand <marchandyohannnnn@gmail.com>
2023-02-02 10:50:53 +0100

    refactor: 💡 Add more tuning mode

--- content/tuning.js
+++ content/tuning.js
@@ -1,7 +1,15 @@
 export default {
-  "Standard": ["E2", "A2", "D3", "G3", "B3", "E4"],
-  /*"1 ton": "DGCFAD",
-  "Drop d": "DADGBE",
-  "Double dropped d": "DADGBD",
-  "Open de d": "DADFAD",*/
+  'Standard': ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
+  'Drop A': ['A2', 'E2', 'A3', 'D3', 'F#3', 'B4' ],
+  'Drop B': [ 'B2', 'F#2', 'B3', 'E3', 'G#3', 'C#4'],
+  'Drop C': [ 'C2', 'G2', 'C3', 'F3', 'A3', 'D4'],
+  'Drop D': ['D2', 'A2', 'D3', 'G3', 'B3', 'E4'],
+  'Open A': ['E2','A2','C#3','E3','A3','E4'],
+  'Open B': ['B2','F#2','B3','F#3','B3','D#4'],
+  'Open C': ['C2','G2','C3','G3','C3','E4'],
+  'Open D': ['D2','A2','C3','F#3','A3','D4'],
+  'Open E': ['E2','B2','E3','G#3','B3','E4'],
+  'Open F': ['F2','A2','C3','F3','C3','F4'],
+  'Open G': ['D2','G2','D3','G3','B3','D4'],
+  'DADGAD': ['D2','A2','D3','G3','A3','D4'],
 }
